579cb07115201b9da83dee55ae92a22651f72e31
Valerio Mario Casale <valeriomc@MacBook-Pro-di-Valerio.local>
2024-03-24 22:20:45 +0100

    Configure docker compose to wait for db

--- README.md
+++ README.md
@@ -1,12 +1,27 @@
 # recipe-app-api
 
+### Build docker image
+
+docker build .
+
 docker-compose build 
 
 ## Create project into our container
 
 docker-compose run --rm app sh -c "django-admin startproject app ."
 
+docker-compose run --rm app sh -c "django-admin startapp core"
 
 ## Run project
 
 docker-compose up
+
+## Update changes
+
+docker-compose down
+docker-compose build
+
+
+## Run tests
+
+docker-compose run --rm app sh -c "python manage.py test"
---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,26 @@
+"""
+Django command to wait for the database to be available
+"""
+from sqlite3 import OperationalError
+import time
+
+from psycopg2 import OperationalError as Psycopg20pError
+
+from typing import Any
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    """Django wait for database"""
+
+    def handle(self, *args: Any, **options: Any):
+        self.stdout.write('Waiting for database...')
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases = ['default'])
+                db_up = True
+            except (Psycopg20pError, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database available!'))
